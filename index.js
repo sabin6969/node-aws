68cdfe37476170774130f8a93a750dfb3d888bbb
sabin4646 <sabinp@cspventuresltd.com>
2025-05-02 21:16:53 +0530

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+    res.status(200).json({ sucess: true, message: "Hello World", });
+})
+
+app.listen(4000, () => {
+    console.log("Server is up and running at port number 4000");
+})
