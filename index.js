c298e9e66b78cce8aac12fb13b19d9003734bbfb
sabin4646 <sabinp@cspventuresltd.com>
2025-05-08 19:07:42 +0530

    fix: correct spelling of 'success' in response messages

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/hello", (req, res) => {
-    res.status(200).json({ sucess: true, message: "Hello World", });
+    res.status(200).json({ success: true, message: "Hello World", });
 })
 
 app.get("/", (req, res) => {
-    res.status(200).json({ sucess: true, message: "NRB MKC" });
+    res.status(200).json({ success: true, message: "NRB MKC" });
 })
 
 app.listen(4000, () => {
