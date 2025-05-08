9737e5c0983414d2029eaf4be5478414cf3617b7
sabin4646 <sabinp@cspventuresltd.com>
2025-05-08 18:58:58 +0530

    fix: correct endpoint definition for root path

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/hello", (req, res) => {
     res.status(200).json({ sucess: true, message: "Hello World", });
 })
 
+app.get("/", (req, res) => {
+    res.status(200).json({ sucess: true, message: "NRB MKC" });
+})
+
 app.listen(4000, () => {
     console.log("Server is up and running at port number 4000");
 })
